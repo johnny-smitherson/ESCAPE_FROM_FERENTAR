960ed42f07c95d58dec4c01e6fd0abf70d6c7a34
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2024-12-06 23:44:44 +0200

    bugs

--- client/src/comp.rs
+++ client/src/comp.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 #[allow(non_snake_case)]
 use std::{fmt::Display, str::FromStr};
 
@@ -8,11 +9,12 @@ use dioxus::prelude::*;
 use dioxus_elements::geometry::{euclid::Size2D, WheelDelta};
 use dioxus_logger::tracing::{error, info, warn};
 use serde::{Deserialize, Serialize};
+use server_fn::error::{NoCustomError, ServerFnErrorErr};
 
 use crate::_const::{MAX_Z, MIN_Z, REF_Z};
 
 
-fn get_tile_positions(pos: (f64, f64), zoom: f64, dimensions: (f64, f64), max_pic_pixels: f64) -> Vec<(i32, i32, i32)> {
+fn get_tile_positions_one_level(pos: (f64, f64), zoom: f64, dimensions: (f64, f64), max_pic_pixels: f64) -> Vec<(i32, i32, i32)> {
     let vmin_px = f64::min(dimensions.0, dimensions.1);
     let min_dim_tiles = vmin_px / max_pic_pixels;
     let ideal_tile_level = (f64::trunc(f64::log2(min_dim_tiles) + zoom) as i32).clamp(MIN_Z, MAX_Z);
@@ -37,6 +39,18 @@ fn get_tile_positions(pos: (f64, f64), zoom: f64, dimensions: (f64, f64), max_pi
     ze_squarez.dedup();
     ze_squarez
 }
+
+fn get_tile_positions(pos: (f64, f64), zoom: f64, dimensions: (f64, f64)) -> Vec<(i32, i32, i32)> {
+    let mut ze_squarez = get_tile_positions_one_level(pos, zoom, dimensions, 512.0);
+    let mut ze_squarez_big = get_tile_positions_one_level(pos, zoom, dimensions, 256.0);
+    // info!("first grp: z={} num={}", ze_squarez[0].2, ze_squarez.len());
+    // info!("second grp: z={} num={}", ze_squarez_big[0].2, ze_squarez_big.len());
+    ze_squarez.append(&mut ze_squarez_big);
+    ze_squarez.sort();
+    ze_squarez.dedup();
+    ze_squarez
+}
+
 #[component]
 pub fn MapsDisplay (
     zoom: ReadOnlySignal<f64>,
@@ -44,15 +58,93 @@ pub fn MapsDisplay (
     dimensions: ReadOnlySignal<(f64, f64)>,
 ) -> Element {
 
-    // screen must contain at least this many images
+    let ze_squarez = use_memo(move || {
+        let sq =         get_tile_positions(*pos.read(), *zoom.read(), *dimensions.read());
+        // info!("computed {:?} squares", sq.len());
+        sq
+    });
+    let mut square_storage = use_signal(HashMap::<(i32,i32,i32),String>::new);
+    // info!("SQUARE STORAGE: {}", square_storage.peek().len());
 
-    let mut ze_squarez = get_tile_positions(*pos.read(), *zoom.read(), *dimensions.read(), 512.0);
-    let mut ze_squarez_big = get_tile_positions(*pos.read(), *zoom.read(), *dimensions.read(), 256.0);
-    // info!("first grp: z={} num={}", ze_squarez[0].2, ze_squarez.len());
-    // info!("second grp: z={} num={}", ze_squarez_big[0].2, ze_squarez_big.len());
-    ze_squarez.append(&mut ze_squarez_big);
-    ze_squarez.sort();
-    ze_squarez.dedup();
+    let mut fut = use_future(move || async move {
+        let _total_count = ze_squarez.len();
+        let request_list = ze_squarez.read().iter().filter(|k| !square_storage.peek().contains_key(k)).cloned().collect::<Vec<_>>();
+        let _new_conut = request_list.len();
+        if _new_conut == 0 {
+            return;
+        }
+        info!("future started for {_new_conut} imgs new / {_total_count} total");
+        match  get_tile_list(request_list.clone()).await {
+            Ok(x) => {
+                // set empty string in dict
+                for tile in request_list.iter().cloned() {
+                    if !square_storage.peek().contains_key(&tile) {
+                        square_storage.write().insert(tile, "".to_string());
+                    }
+                }
+                let mut stream = x.into_inner();
+                use futures_util::stream::StreamExt;
+                let mut i = 0;
+                let mut buf = "".to_string();
+                while let Some(Ok(chunk)) = stream.next().await {
+                    buf.push_str(&chunk);
+                    let mut lines = vec![];
+                    while let Some(newline_pos) = buf.find("\n") {
+                        lines.push(buf[..newline_pos].to_string());
+                        buf = buf[(newline_pos+1)..].to_string();
+                    }
+                    for line in lines {
+                        let fields :Vec<_> = line.splitn(5, "|").collect();
+                        // info!("FIELDS: {:?}", fields);
+                        let is_ok = fields[0] == "ok";
+                        let sq_z = fields[1].parse().unwrap_or(0);
+                        let sq_x = fields[2].parse().unwrap_or(0);
+                        let sq_y = fields[3].parse().unwrap_or(0);
+                        let body = fields[4];
+                        if is_ok {
+                            // only write if we've got a `""`, otherwise it's been cancelled
+                            if square_storage.peek().get(&(sq_z, sq_x, sq_y)) == Some(&"".to_string()) {
+                                square_storage.write().insert((sq_z, sq_x, sq_y), body.to_string());
+                                async_std::task::sleep(std::time::Duration::from_millis(1)).await;
+                            } else {
+                                warn!("cancelled but got response: {:?}", (sq_z, sq_x, sq_y));
+                            }
+                            // break;
+                        } else {
+                            warn!("stream err:  img z={sq_z}/x={sq_x}/y={sq_y}: {body}");
+                        }
+                        i += 1;
+                        if i == request_list.len() {
+                            info!("client finished all {} tiles.", i);
+                            break;
+                        }
+                    }
+                }
+            }
+            Err(x) => {
+                warn!("err fetching img list from  server: {:?}", x)
+            }
+        }
+    });
+
+    use_effect(move || {
+        let z2 = ze_squarez.read().len();
+        if z2 > 0 {
+            fut.pause();
+            fut.cancel();
+            // clear unused keys
+            let zl = std::collections::HashSet::<(i32,i32,i32)>::from_iter(ze_squarez.peek().iter().cloned());
+            let to_delete = square_storage.peek().keys().filter(|k| !zl.contains(k)).cloned().collect::<Vec<_>>();
+
+            if !to_delete.is_empty() {
+                for d in to_delete {
+                    square_storage.write().remove(&d);
+                }
+            }
+
+            fut.restart(); 
+        }
+    });
 
     rsx! {
         h3 { "zoom = {zoom:?} pos = {pos:?}" }
@@ -60,12 +152,16 @@ pub fn MapsDisplay (
             id:"main_display_list",
             style:"list-style-type:none;margin:0;padding:0;",
 
-            for (sq_z, sq_x, sq_y) in ze_squarez {
+            for (sq_z, sq_x, sq_y) in ze_squarez.read().iter().cloned() {
                 li {
                     key: "tile_li_{sq_z}_{sq_x}_{sq_y}",
                     MapsTile {
                         zoom, pos, dimensions,
                         sq_x, sq_y, sq_z,
+                        square_storage,
+                        // src: //use_memo(move || {
+                            
+                        //}.into())
                     }
                 }
             }
@@ -82,6 +178,8 @@ fn MapsTile (
     pos: ReadOnlySignal<(f64, f64)>,
     dimensions: ReadOnlySignal<(f64, f64)>,
     sq_x: i32, sq_y: i32, sq_z: i32,
+    // src: ReadOnlySignal<String>,
+    square_storage: ReadOnlySignal<HashMap::<(i32,i32,i32),String>>,
 ) -> Element {
     let tile_size_abs = f64::exp2(REF_Z - sq_z as f64);
     let tile_pos_abs = (sq_x as f64 * tile_size_abs , sq_y as f64 * tile_size_abs );
@@ -95,17 +193,9 @@ fn MapsTile (
         tile_relative.1 /  camera_zoom,
     );
     let tile_size = tile_size_abs / camera_zoom;
-    let mut src = use_signal(|| "".to_string());
-    use_future(move || async move {
-        match  get_server_tile_img(sq_z, sq_x, sq_y).await {
-            Ok(x) => {
-                *src.write()=x;
-            }
-            Err(x) => {
-                warn!("err fetching img z={sq_z}/x={sq_x}/y={sq_y}:  {:?}", x)
-            }
-        }
-    });
+
+    let src = use_memo(move || square_storage.read().get(&(sq_z, sq_x, sq_y)).cloned().unwrap_or("".to_string()));
+
     rsx! {
         img { 
             id: "tile_img_{sq_z}_{sq_x}_{sq_y}",
@@ -123,14 +213,79 @@ fn MapsTile (
     }
 }
 
-#[server(GetServerTile)]
-async fn get_server_tile_img(sq_z: i32, sq_x: i32, sq_y: i32) -> Result<String, ServerFnError> {
+// #[server(GetServerTile)]
+use server_fn::codec::StreamingText;
+use server_fn::codec::TextStream;
+
+#[server(output = StreamingText)]
+async fn get_tile_list(list: Vec<(i32,i32,i32)>) -> Result<TextStream, ServerFnError> {
+    let (mut tx, rx) = async_channel::bounded(1);
+    tokio::spawn(async move {
+        use futures::stream::FuturesUnordered;
+        let list_len = list.len();
+        info!("server: feteching {} img", list_len);
+        let mut futures = FuturesUnordered::from_iter(list.into_iter().map(get_server_tile_img));
+        use futures_util::StreamExt;
+
+        let mut success_count = 0;
+        let mut err_count = 0;
+        while let Some((coord, result)) = futures.next().await {
+            let msg = match result {
+                Ok(result) => {
+                    success_count += 1;
+                    format!("ok|{}|{}|{}|{}\n", coord.0, coord.1, coord.2, result)
+
+                },
+                Err(err) => {
+                    err_count += 1;
+                    format!("err|{}|{}|{}|{}\n", coord.0, coord.1, coord.2, err)
+                }
+            };
+            
+            let _x = tx.send(Ok(msg)).await;
+            if let Err(err) = _x {
+                warn!("cut streaming {} img / {} planned: {:?}",  success_count+err_count, list_len, err);
+                tx.close();
+                return;
+            }
+            
+        }
+        info!("done streaming {} img: {} success  / {} fail.", list_len, success_count, err_count);
+    });
+
+    Ok(TextStream::new(rx))
+}
+
+async fn get_server_tile_img(coord: (i32,i32,i32)) -> ((i32,i32,i32), Result<String, ServerFnError>) {
+    const RETRIES: u32 = 4;
+    for x in 1..=RETRIES {
+        match get_server_tile_img_once(coord).await {
+            Ok(r) => {return (coord, Ok(r));},
+            Err(r) => {
+                if x == RETRIES {
+                    return (coord, Err(r));
+                }
+                let sleep_ms = x as u64 * 250 * 2_u64.pow(x);
+                // info!("failed to get tile img; chance {x}/{RETRIES}; sleep {sleep_ms}ms");
+                tokio::time::sleep(tokio::time::Duration::from_millis(sleep_ms)).await;
+                continue;
+            }
+        }
+    }
+    unreachable!();
+}
+
+async fn get_server_tile_img_once(coord: (i32,i32,i32)) -> Result<String, ServerFnError> {
+    let (sq_z, sq_x, sq_y) = coord;
     let url = format!("http://localhost:8000/api/tile/google_hybrid/{sq_z}/{sq_x}/{sq_y}/jpg");
 
     let response = reqwest::get(&url).await?;
     let status_code = response.status().clone();
     let content_type = response.headers()["Content-Type"].to_str()?.to_string();
-    let content_length = response.content_length().clone();
+    if !status_code.is_success() ||  content_type.len() < 4 {
+        return Err(ServerFnError::new(format!("bad response from tile server: {:?}", status_code)));
+    }
+    
     let resp_bytes = response.bytes().await?;
     let resp_base64 = base64::prelude::BASE64_STANDARD.encode(resp_bytes);
 
--- client/src/main.rs
+++ client/src/main.rs
@@ -34,7 +34,35 @@ enum Route {
         url_hash: MapState,
     },
     #[route("/hello")]
-    Hello{}
+    Hello{},
+    #[route("/storage")]
+    Storage{}
+}
+#[component]
+fn Storage() -> Element {
+    let mut count_session = dioxus_sdk::storage::use_singleton_persistent(|| 0);
+    let mut count_local = dioxus_sdk::storage::use_synced_storage:: <dioxus_sdk::storage::LocalStorage,i32>("synced".to_string(), || 0);
+
+    rsx!(
+        div {
+            button {
+                onclick: move |_| {
+                    *count_session.write() += 1;
+                },
+                "Click me!"
+            },
+            "I persist for the current session. Clicked {count_session} times"
+        }
+        div {
+            button {
+                onclick: move |_| {
+                    *count_local.write() += 1;
+                },
+                "Click me!"
+            },
+            "I persist across all sessions. Clicked {count_local} times"
+        }
+    )
 }
 
 
@@ -43,32 +71,80 @@ enum Route {
 fn Hello() -> Element {
     let mut response = use_signal(String::new);
     use futures_util::StreamExt;
+    let cancel = use_signal(|| tokio::sync::broadcast::channel::<()>(1));
+    let cancel_ack = use_signal(|| async_channel::bounded::<()>(1));
+    let mut fut = use_future(move || {
+        async move {
+            info!("future started.");
+            response.write().clear();
+            if let Ok(stream) = test_stream().await {
+                response.write().push_str("Stream started\n");
+                let mut stream = stream.into_inner();
+                let mut cancel_rx = cancel.peek().0.subscribe();
+                let cancel_ack_tx = cancel_ack.peek().0.clone();
+                let cancel_future = cancel_rx.recv();
+                futures::pin_mut!(cancel_future);
+                loop {
+                    cancel_future = match futures::future::select(stream.next(), cancel_future).await {
+                        futures::future::Either::Left((stream_value, _next_fut)) => {
+                            if let Some(Ok(text)) = stream_value {
+                                response.write().push_str(&text);
+                                _next_fut
+                            } else {
+                                info!("stream finished.");
+                                return;
+                            }
+                        }
+                        futures::future::Either::Right((_stop_value, _)) => {
+                            if let Err(e) = cancel_ack_tx.send(()).await {
+                                error!("failed to send cancel ack message: {:?}", e);
+                            }
+                            info!("cancel channel got message: cancelling stream.");
+                            // async_std::task::sleep(std::time::Duration::from_millis(1000)).await;
+                            return;
+                        }
+                    }
+                }
+            }
+    }});
+
     rsx! {
         button {
             onclick: move |_| async move {
-                response.write().clear();
-                if let Ok(stream) = test_stream().await {
-                    response.write().push_str("Stream started\n");
-                    let mut stream = stream.into_inner();
-                    while let Some(Ok(text)) = stream.next().await {
-                        response.write().push_str(&text);
-                    }
+                info!("button clicked.");
+                if let Err(e) = cancel.peek().0.clone().send(()) {
+                    warn!("cancel failed!!! {:?}", e);
                 }
+                info!("cancel sent, waiting for resp...");
+                if let Err(e) = cancel_ack.peek().1.recv().await {
+                    warn!("cancel_ack read failed: {:?}!", e);
+                }
+                info!("cancel ack received.");
+                fut.cancel();
+                fut.restart();
+                info!("future restarted.");
             },
-            "Start stream"
+            "Cancel & Restart stream"
         }
-        "{response}"
+        pre {
+            "{response}"
+        }
     }
 }
 use crate::server_fn::codec::StreamingText;
 use crate::server_fn::codec::TextStream;
 #[server(output = StreamingText)]
 pub async fn test_stream() -> Result<TextStream, ServerFnError> {
-    let (tx, rx) = futures::channel::mpsc::unbounded();
+    // let (tx, rx) = async_channel::bounded(1);
+    let (mut tx, rx) = futures::channel::mpsc::unbounded();
     tokio::spawn(async move {
         loop {
             tokio::time::sleep(tokio::time::Duration::from_millis(500)).await;
-            let _ = tx.unbounded_send(Ok("Hello, world!".to_string()));
+            if let Err(_) = tx.unbounded_send(Ok("Hello, world!\n".to_string())) {
+                warn!(" test_stream() : stream close");
+                tx.close_channel();
+                return;
+            }
         }
     });
 
@@ -80,22 +156,23 @@ pub async fn test_stream() -> Result<TextStream, ServerFnError> {
 #[component]
 fn Home(url_hash: ReadOnlySignal<MapState>) -> Element {
     // The initial state of the state comes from the url hash
-    let _init_state = (&*url_hash)();
+    let mut _init_state = (&*url_hash)();
     // if url is invalid, they will not match
     if !_init_state.is_init {
         warn!("redirecting from invalid url_hash into default...");
+        _init_state = INIT_STATE;
         navigator().replace(Route::Home {
             url_hash: INIT_STATE,
         });
     }
 
-    let mut state = use_signal(&*url_hash);
+    let mut state = use_signal(|| _init_state.clone());
     let mut zoom = use_signal(|| state.peek().zoom);
     let mut pos = use_signal(|| state.peek().pos);
     let dimensions: Signal<(f64, f64)> = use_signal(|| (0.0, 0.0));
 
     // Change the state signal when the url hash changes
-    use_memo(move || {
+    use_effect(move || {
         if *state.peek() != *url_hash.read() {
             state.set(url_hash());
         }
@@ -106,33 +183,33 @@ fn Home(url_hash: ReadOnlySignal<MapState>) -> Element {
         dioxus_sdk::utils::timing::use_debounce(std::time::Duration::from_millis(100), move |_| {
             navigator().replace(Route::Home { url_hash: state() });
         });
-    use_memo(move || {
+        use_effect(move || {
         if *state.read() != *url_hash.peek() {
             debounce_write_url.action(());
         }
     });
 
     // change zoom when state changes
-    use_memo(move || {
+    use_effect(move || {
         if *zoom.peek() != (*state.read()).zoom {
             zoom.set(state.read().zoom);
         }
     });
     // change state when zoom changes
-    use_memo(move || {
+    use_effect(move || {
         if *zoom.read() != (*state.peek()).zoom {
             state.write().zoom = *zoom.read();
         }
     });
 
     // change pos when state changes
-    use_memo(move || {
+    use_effect(move || {
         if *pos.peek() != (*state.read()).pos {
             pos.set(state.read().pos);
         }
     });
     // change state when pos changes
-    use_memo(move || {
+    use_effect(move || {
         if *pos.read() != (*state.peek()).pos {
             state.write().pos = *pos.read();
         }
